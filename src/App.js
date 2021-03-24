9b5ccdf2555cce5c01a1f79eab8d32f6b7a8021a
mbalakiran <balakiranmanthri1995@gmail.com>
2021-03-24 20:39:58 +0100

    Second. Completed Every thing Just Company left

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
 // import logo from './logo.svg';
 import './App.css';
-import {useState, useEffect} from "react";
+import Home from './Home';
+import {BrowserRouter, Route} from "react-router-dom";
+import UserDetails from "./UserDetails";
+import Navbar from "./Navbar";
 
-function App() {
-  const[userData, setUserData] = useState([]);
-
-  const url = "https://jsonplaceholder.typicode.com/users"
-
-  useEffect(() => {
-    fetch(url).then((response) => response.json())
-    .then((userData) => setUserData(userData));
-  },[]);
-
+export default function App() {
   return (
-    // <div className="App">
-    <>
-      <ul className="outbox">
-        {userData.map(el => (
-          <section className="box">
-            <button className="f" key={el.id}>{el.name.charAt(0)}</button>
-            <h2 className = "h" key={el.id}>{el.name}</h2>
-            <p className = "p" key={el.id}>@{el.username}</p>
-            <span className ="e" key={el.id}>http://{el.email}</span>
-            <br></br>
-            <p>
-              <button className="button">MORE DETAILS</button>
-            </p>
-          </section>
-        ))}
-      </ul>
-      {/* <h1>Hello</h1> */}
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route path="/userdetails/:id" component={UserDetails} />
+    </BrowserRouter>
   );
 }
 
-export default App;
+// import {useState, useEffect} from "react";
+
+// function App() {
+//   const[userData, setuserData] = useState([]);
+
+//   const url = "https://jsonplaceholder.typicode.com/users"
+
+//   useEffect(() => {
+//     fetch(url).then((response) => response.json())
+//     .then((userData) => setuserData(userData));
+//   },[]);
+
+//   return (
+//     // <div className="App">
+//     <>
+//       <ul className="outbox">
+//         {userData.map(el => (
+//           <section className="box">
+//             <button className="f" key={el.id}>{el.name.charAt(0)}</button>
+//             <h2 className = "h" key={el.id}>{el.name}</h2>
+//             <p className = "p" key={el.id}>@{el.username}</p>
+//             <span className ="e" key={el.id}>http://{el.email}</span>
+//             <br></br>
+//             <p>
+//               <button className="button">MORE DETAILS</button>
+//             </p>
+//           </section>
+//         ))}
+//       </ul>
+//       {/* <h1>Hello</h1> */}
+//     </>
+//   );
+// }
+
+// export default App;
